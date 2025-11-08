f02fafc5f4e40e0e4366f19980c3968513556861
Behnoush <behnoush.shahraeini@gmail.com>
2025-11-08 11:08:03 +0330

    Update Algorithm

---
+++ MultiplyByTen.py
@@ -0,0 +1,6 @@
+# Multiply By Ten
+# - Return a list with each number multiplied by 10.
+# - Example: [1,2,3] → [10,20,30]
+
+def multiplyByTen(nums: list[int]) -> list[int]:
+    return [x * 10 for x in nums]
